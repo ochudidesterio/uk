4ac88a2f3e9e024236d4cbfb769458626cc3fc89
Desterio Ochudi <45032910+ochudidesterio@users.noreply.github.com>
2021-03-10 02:28:51 +0300

    Add files via upload

---
+++ ukulima-tv-web-master/README.old.md
@@ -0,0 +1,1 @@
+# ukulima-tv-web
---
+++ ukulima-tv-web-master/src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react"
+import './footer.css'
+
+const Footer=()=>{
+    return(
+        <>
+        <div className="footer">
+            <div className="about-us">
+                <p>About Us</p>
+            </div>
+            <div className="contact-us">
+                <p>Contact Us</p>
+            </div>
+            <div className="social-media">
+                <p>Social media</p>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Footer
